2c068dfef2380336ee2b5c66693cfaaf12f1fe98
aryanpachchigar <aryanpachchigar12@gmail.com>
2025-04-15 13:57:45 +0530

    flairupdated

--- src/components/loading/loading.jsx
+++ src/components/loading/loading.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Spin } from "antd";
 
 const Loading = () => {
   return (
     <div className="flex items-center justify-center h-[80vh]">
-      <div className="border-4 border-solid border-gray-200 border-t-cyan-600 rounded-full w-12 h-12 animate-bounce"></div>
+      <Spin size="large" tip="Loading..." />
     </div>
   );
 };
